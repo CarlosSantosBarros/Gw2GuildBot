671f6d03e7683197315a0dd5d92a60a6e001d125
MasterRefactor <31740094+MasterRefactor@users.noreply.github.com>
2022-01-05 15:44:21 +0000

    eslint and deleted archived files

--- src/archived/events/guildDelete.js
+++
@@ -1,4 +0,0 @@
-const { log } = require("../util");
-module.exports = guild => {
-  log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-};
--- src/archived/modelUser.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("User", {
-    snowflake: {
-      type: DataTypes.UUID,
-      unique: true,
-    },
-  });
-};
